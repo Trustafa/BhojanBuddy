aed4455bf02c481c520315a7a5f748a6cef99340
MustafaSalim <mustafa.salman5252@gmail.com>
2023-11-04 14:36:06 +0000

    Change of spelling mistake

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import Basket from "./Routes/Basket";
 import Home from "./Routes/Home";
-import Receipes from './Routes/Receipes';
+import Recipes from './Routes/Recipes';
 import Support from './Routes/Support';
 import UserSettings from './Routes/UserSettings';
 import MainMenu from "./MainMenu"
-import { BrowserRouter as Router, Routes, Route }
-    from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React from 'react';
 
 function App() {
@@ -18,7 +17,7 @@ function App() {
               <Route exact path='/' element={<Home />} />
               <Route path='/settings' element={<UserSettings />} />
               <Route path='/contact' element={<Support />} />
-              <Route path='/receipes' element={<Receipes />} />
+              <Route path='/recipes' element={<Recipes />} />
               <Route path='/basket' element={<Basket />} />
           </Routes>
       </Router>
