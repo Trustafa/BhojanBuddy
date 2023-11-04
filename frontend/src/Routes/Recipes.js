f0e82d0664f63740551816a6c46ae7e06d3a1e56
MustafaSalim <mustafa.salman5252@gmail.com>
2023-11-04 20:18:12 +0000

    Tabs working Recipe page

--- frontend/src/Routes/Recipes.js
+++ frontend/src/Routes/Recipes.js
@@ -1,10 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Recipes.css'
 const Recipes = () => {
     return (
-        <div className="Recipes">
-            <h1>Recipes</h1>
-        </div>
+        <Tabs/>
     );
 };
 
-export default Recipes;
+const Tabs = () => {
+    const [activeTab, setActiveTab] = useState("tab1");
+    const handleTab1 = () => {
+        setActiveTab("tab1");
+      };
+    const handleTab2 = () => {
+        setActiveTab("tab2");
+      };
+    return (
+        <div className="Tabs">
+          {/* Tab nav */}
+          <ul className="nav">
+            <li className={activeTab === "tab1" ? "active" : ""}
+                onClick = {handleTab1}>Tab 1</li>
+            <li className={activeTab === "tab2" ? "active" : ""} 
+                onClick = {handleTab2}>Tab 2</li>
+          </ul>
+          <div className="outlet">
+            {activeTab === "tab1" ? <FirstTab /> : <SecondTab />}
+          </div>
+        </div>
+      );
+}
+
+const FirstTab = () => {
+    return (
+      <div className="FirstTab">
+        <p>First Tab!! Hurray!!</p>
+        {/* First tab content will go here */}
+      </div>
+    );
+};
+
+const SecondTab = () => {
+    return (
+      <div className="SecondTab">
+        <p>Second Tab!! Hurray!!</p>
+        {/* Second  tab content will go here */}
+      </div>
+    );
+};
+
+export default Recipes; 
